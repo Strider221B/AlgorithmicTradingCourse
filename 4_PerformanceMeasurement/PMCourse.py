337efa8177de8896fa1e716e6204ea47cd34f0f5
Somesh Chatterjee <somesh.chatterjee@gmail.com>
2021-03-12 05:31:54 +0000

    AnnualizedVolatility

--- 4_PerformanceMeasurement/PMCourse.py
+++ 4_PerformanceMeasurement/PMCourse.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def CAGR(DF):
     "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
     df = DF.copy()
@@ -6,3 +8,10 @@ def CAGR(DF):
     n = len(df)/252
     CAGR = (df["cum_return"][-1])**(1/n) - 1
     return CAGR
+
+def volatility(DF):
+    "function to calculate annualized volatility of a trading strategy"
+    df = DF.copy()
+    df["daily_ret"] = DF["Adj Close"].pct_change()
+    vol = df["daily_ret"].std() * np.sqrt(252)
+    return vol
--- 4_PerformanceMeasurement/PerformanceMeasurements.py
+++ 4_PerformanceMeasurement/PerformanceMeasurements.py
@@ -11,3 +11,10 @@ class PerformanceMeasurements:
         no_of_years = len(df) / 252
         cagr = ((cum_return[-1])**(1/no_of_years)) - 1
         return cagr
+    
+    @staticmethod
+    def get_annualized_volatility(df: pd.DataFrame) -> float:
+        daily_ret = df['Adj Close'].pct_change().values
+        daily_ret = daily_ret[1:]
+        volaility  = daily_ret.std() * np.sqrt(252)
+        return volaility
