44c282f90f04d4ba88dd63be3d9baacaac9581a6
Somesh Chatterjee <somesh.chatterjee@gmail.com>
2021-03-14 06:55:10 +0000

    FixedResistanceBreakout

--- 5_BackTesting/ResistanceBreakout.py
+++ 5_BackTesting/ResistanceBreakout.py
@@ -26,17 +26,19 @@ class ResistanceBreakout:
                         elif low_less_than_roll_min_cp:
                             tickers_signal[ticker] = 'sell'
                 elif tickers_signal[ticker] == 'buy':
-                    if ohlc['Low'][i] < ohlc['Close'][i-1] - ohlc['ATR'][i-1]:
+                    exit_price = (ohlc['Close'][i-1] - ohlc['ATR'][i-1])
+                    if ohlc['Low'][i] < exit_price:
                         tickers_signal[ticker] = ''
-                        tickers_return[ticker].append(((ohlc['Close'][i-1] - ohlc['ATR']) / ohlc_dict[ticker]['Close'][i-1])-1)
+                        tickers_return[ticker].append((exit_price / ohlc['Close'][i-1])-1)
                     else:
                         tickers_return[ticker].append((ohlc['Close'][i]/ohlc['Close'][i-1])-1)
                         if low_less_than_roll_min_cp and vol_greater_than_threshold:
                             tickers_signal[ticker] = 'sell'
                 else:
-                    if ohlc['High'][i] < ohlc['Close'][i-1] + ohlc['ATR'][i-1]:
+                    exit_price = ohlc['Close'][i-1] + ohlc['ATR'][i-1]
+                    if ohlc['High'][i] > exit_price:
                         tickers_signal[ticker] = ''
-                        tickers_return[ticker].append((ohlc['Close'][i-1]/(ohlc['Close'][i-1] + ohlc['ATR'][i-1])) - 1)
+                        tickers_return[ticker].append((ohlc['Close'][i-1]/exit_price) - 1)
                     else:
                         tickers_return[ticker].append((ohlc['Close'][i-1] / ohlc['Close'][i]) - 1)
                         if high_greater_than_roll_max_cp and vol_greater_than_threshold:
